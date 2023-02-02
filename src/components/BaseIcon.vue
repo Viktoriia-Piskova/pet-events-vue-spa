d965f4eaf8a0b78fb0ab367128f3521f67ee0c7c
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2023-02-02 19:58:43 +0200

    Get and render events from api

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="icon-wrapper">
+    <span v-html="svg"> </span>
+    <span class="icon-description">
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script>
+import feather from "feather-icons";
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "users",
+    },
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+  },
+  computed: {
+    svg() {
+      return feather.icons[this.name].toSvg({
+        class: "icon",
+        width: this.width,
+        height: this.height,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+  margin-right: 6px;
+  gap: 10px;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: #39b982;
+}
+.icon-description {
+  color: rgb(76 73 73 / 99%);
+  font-size: 1rem;
+  font-weight: 700;
+}
+</style>
--- src/views/EventsList.vue
+++ src/views/EventsList.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="about">
     <h1>Event list</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard.vue";
+import axios from "axios";
 export default {
   name: "EventsList",
+  data() {
+    return {
+      events: [],
+    };
+  },
   components: { EventCard },
+  created() {
+    axios
+      .get(
+        "https://my-json-server.typicode.com/Viktoriia-Piskova/mock-database-vue2/events"
+      )
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((err) => console.log(err));
+  },
 };
 </script>
 
