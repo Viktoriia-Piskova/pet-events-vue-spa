d67322b25904a50800f0badc605899b1c12ac6c3
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2023-02-27 18:50:24 +0200

    package reinstall

--- src/components/EventShow.vue
+++ src/components/EventShow.vue
@@ -6,7 +6,9 @@
       <h5>Organized by {{ event.organizer }}</h5>
       <h5>Category: {{ event.category }}</h5>
     </div>
-    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+    <BaseIcon name="map">
+      <h2>Location</h2>
+    </BaseIcon>
     <address>{{ event.location }}</address>
     <h2>Event details</h2>
     <p>{{ event.description }}</p>
@@ -17,13 +19,9 @@
       }}</span>
     </h2>
     <ul class="list-group">
-      <li
-        v-for="(attendee, index) in event.attendees"
-        :key="index"
-        class="list-item"
-      >
+      <!-- <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
         <b>{{ attendee.name }}</b>
-      </li>
+      </li> -->
     </ul>
   </div>
 </template>
@@ -47,23 +45,27 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.location {
+<style scoped>
+/* .location {
   margin-bottom: 0;
 }
-.location > .icon {
+
+.location>.icon {
   margin-left: 10px;
 }
-.event-header > .title {
+
+.event-header>.title {
   margin: 0;
 }
+
 .list-group {
   margin: 0;
   padding: 0;
   list-style: none;
 }
-.list-group > .list-item {
+
+.list-group>.list-item {
   padding: 1em 0;
   border-bottom: solid 1px #e5e5e5;
-}
+} */
 </style>
